f833239ae9897d0c8e644d2307aa2342d98f047c
Lia <sethroeback1@gmail.com>
2026-04-03 13:34:38 -0600

    started selection sort

--- SelectionSort/SelectionSort.h
+++ SelectionSort/SelectionSort.h
@@ -9,6 +9,13 @@ template <typename T>
 class SelectionSort{
     public:
     std::vector<T> Sort(std::vector<T> arr){
-        
+        int *lowestNumber = new int;
+        for(int i = 0; i < arr.size(); i++){
+            for(int j = 0; j < (arr.size - i); i++){
+                if(arr[j + i] < arr[lowestNumber]) lowestNumber = j;
+
+            }
+            
+        }
     }
 };
